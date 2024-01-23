997364ad9da7f6452dd50538e231b1d2d9301e2f
kimdg1105 <kimss7334@naver.com>
2024-01-23 17:42:19 +0900

    feat: 타이머 상태 관리 추가, 상태별 event 정의 및 비지니스 로직 추가

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/status/timer_status.dart';
+import 'package:sprintf/sprintf.dart';
 
 class TimerScreen extends StatefulWidget {
   @override
@@ -6,21 +10,116 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
+  static const WORK_SECONDS = 25;
+  static const REST_SECONDS = 5;
+
+  late TimerStatus _timerStatus;
+  late int _seconds;
+  late int _pomodoroCount;
+
+  @override
+  void initState() {
+    super.initState();
+    _timerStatus = TimerStatus.stopped;
+    print(_timerStatus.toString());
+    _seconds = WORK_SECONDS;
+    _pomodoroCount = 0;
+  }
+
+  void run() {
+    setState(() {
+      _timerStatus = TimerStatus.running;
+      print("[=>]" + _timerStatus.toString());
+      runTimer();
+    });
+  }
+
+  void rest() {
+    setState(() {
+      _seconds = REST_SECONDS;
+      _timerStatus = TimerStatus.resting;
+      print("[=>]" + _timerStatus.toString());
+      runTimer();
+    });
+  }
+
+  void pause() {
+    setState(() {
+      _timerStatus = TimerStatus.paused;
+      print("[=>]" + _timerStatus.toString());
+    });
+  }
+
+  void resume() {
+    run();
+  }
+
+  void stop() {
+    setState(() {
+      _seconds = WORK_SECONDS;
+      _timerStatus = TimerStatus.stopped;
+      print("[=>]" + _timerStatus.toString());
+    });
+  }
+
+  void runTimer() async {
+    Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      switch (_timerStatus) {
+        case TimerStatus.paused:
+          t.cancel();
+          break;
+        case TimerStatus.stopped:
+          t.cancel();
+          break;
+        case TimerStatus.running:
+          if (_seconds <= 0) {
+            print("작업 완료!");
+            rest();
+          } else {
+            setState(() {
+              _seconds--;
+            });
+          }
+          break;
+        case TimerStatus.resting:
+          if (_seconds <= 0) {
+            setState(() {
+              _pomodoroCount++;
+            });
+            print("오늘 $_pomodoroCount 개의 작업 완료!");
+            t.cancel();
+            stop();
+          } else {
+            setState(() {
+              _seconds--;
+            });
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  String secondsToString(int seconds) {
+    return sprintf("%02d:%02d", [seconds ~/ 60, seconds % 60]);
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> _runningButtons = [
       ElevatedButton(
         child: Text(
-          1 == 2 ? '가보자~' : "잠만...",
+          _timerStatus == TimerStatus.paused ? '가보자~' : "잠만...",
           style: TextStyle(color: Colors.white, fontSize: 16),
         ),
         style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
-        onPressed: () {},
+        onPressed: _timerStatus == TimerStatus.paused ? resume : pause,
       ),
       Padding(padding: EdgeInsets.all(20)),
       ElevatedButton(
-        child: Text("에잇~!", style: TextStyle(fontSize: 16)),
-        onPressed: () {},
+        child: Text("", style: TextStyle(fontSize: 16)),
+        onPressed: stop,
       )
     ];
 
@@ -29,14 +128,18 @@ class _TimerScreenState extends State<TimerScreen> {
         child:
             Text("가보자~", style: TextStyle(color: Colors.white, fontSize: 16)),
         style: ElevatedButton.styleFrom(
-            backgroundColor: 1 == 2 ? Colors.green : Colors.grey),
-        onPressed: () {},
+            backgroundColor: _timerStatus == TimerStatus.resting
+                ? Colors.green
+                : Colors.grey),
+        onPressed: run,
       ),
     ];
 
     return Scaffold(
       appBar: AppBar(
         title: Text("뽀모도로 타이머"),
+        backgroundColor:
+            _timerStatus == TimerStatus.resting ? Colors.green : Colors.blue,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -46,7 +149,7 @@ class _TimerScreenState extends State<TimerScreen> {
               width: MediaQuery.of(context).size.width * 0.6,
               child: Center(
                 child: Text(
-                  "00:00",
+                  secondsToString(_seconds),
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 48,
@@ -55,12 +158,14 @@ class _TimerScreenState extends State<TimerScreen> {
               ),
               decoration: BoxDecoration(
                   shape: BoxShape.circle,
-                  color: 1 == 2 ? Colors.green : Colors.blue)),
+                  color: _timerStatus == TimerStatus.resting
+                      ? Colors.green
+                      : Colors.blue)),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: 1 == 2
+            children: _timerStatus == TimerStatus.resting
                 ? const []
-                : 1 == 2
+                : _timerStatus == TimerStatus.stopped
                     ? _stoppedButtons
                     : _runningButtons,
           )
