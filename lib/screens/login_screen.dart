608bd537835e7d11e7d92ac5716adad24a5caba3
kimdg1105 <kimss7334@naver.com>
2024-02-01 21:43:06 +0900

    feat: loginScreen 구현

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatelessWidget {
+  Future setLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLogin', true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('LoginScreen')),
-    );
+    return Scaffold(
+        appBar: AppBar(
+            title: Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                color: Colors.red,
+                child: Center(
+                  child: Text(
+                    "로그인",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ))),
+        body: Center(
+            child: Container(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: ElevatedButton(
+                  child: Text("로그인"),
+                  onPressed: () {
+                    setLogin().then((value) => Navigator.of(context)
+                        .pushReplacement(MaterialPageRoute(
+                            builder: (context) => ListScreen())));
+                  },
+                ))));
   }
 }
